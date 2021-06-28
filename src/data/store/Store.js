fe573c1b2869bc5c2948741b9f595dfa12a06f33
Andrew Ligon <aligon90@gmail.com>
2021-06-28 18:19:31 -0500

    move the property change changes into a base class

--- src/data/store/Store.js
+++ src/data/store/Store.js
@@ -1,11 +1,14 @@
 import Action from './Action';
+import PropertyChangeEmitter from './PropertyChangeEmitter';
 
-export default class DataStore {
+export default class DataStore extends PropertyChangeEmitter {
 	static Action = Action;
 
 	#state = {};
 
 	constructor() {
+		super();
+
 		for (let [key, value] of Object.entries(this)) {
 			if (value.bindStore) {
 				Object.defineProperty(
@@ -13,64 +16,24 @@ export default class DataStore {
 					key,
 					value.bindStore({
 						scope: this,
-						onUpdate: (...args) => this.#updateState(...args),
-						onStart: () => this.#emitChange(key),
-						onError: () => this.#emitChange(key),
-						onFinish: () => this.#emitChange(key),
+						onUpdate: (...args) => this.updateState(...args),
+						onStart: () => this.onChange(key),
+						onError: () => this.onChange(key),
+						onFinish: () => this.onChange(key),
 					})
 				);
 			}
 		}
 	}
 
-	//#region Changes
-	#listeners = new Map();
-
-	#emitChange(keys) {
-		const listeners = new Set();
-
-		for (let key of keys) {
-			const keyListeners = this.#listeners.get(key);
-
-			keyListeners.forEach(k => listeners.add(k));
-		}
-
-		listeners.forEach(l => l());
-	}
-
-	subscribe(key, fn) {
-		if (Array.isArray(key)) {
-			const cleanup = key.map(k => this.subscribe(key));
-
-			return () => cleanup.forEach(c => c());
-		}
-
-		if (!this.#listeners.has(key)) {
-			this.#listeners.set(key, new Set());
-		}
-
-		this.#listeners.get(key).add(fn);
-
-		return () => {
-			const existing = this.#listeners.get(key);
-
-			existing.delete(fn);
-
-			if (existing.size === 0) {
-				this.#listeners.remove(key);
-			}
-		};
-	}
-	//#endregion
-
 	//#region State Updating
-	#updateState(newState) {
+	updateState(newState) {
 		const updated = Object.keys(newState);
 		const merged = this.applyState(newState);
 
 		this.#state = merged;
 
-		this.#emitChange(updated);
+		this.emitChange(updated);
 	}
 
 	applyState(newState) {
