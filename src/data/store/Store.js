76387f1b8e747cbd87fe6b884fa6ed88ce2cf643
Andrew Ligon <aligon90@gmail.com>
2021-06-29 18:09:21 -0500

    initial pass at a hook to get a store instance

--- src/data/store/Store.js
+++ src/data/store/Store.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import { Promises } from '@nti/lib-commons';
 
 import Action from './Action';
@@ -5,13 +7,88 @@ import PropertyChangeEmitter from './PropertyChangeEmitter';
 
 const LifeCycles = ['initialize', 'reload', 'load', 'cleanup'];
 
+const Load = Symbol('Load');
+const Key = Symbol('key');
+const Instances = Symbol('Instances');
+
+function StoreInstanceReducer(state, action) {
+	if (action.type === 'update') {
+		return { store: action.store };
+	}
+
+	return state;
+}
+
 export default class DataStore extends PropertyChangeEmitter {
 	static Action = Action;
 
+	static getStore(key) {
+		const Store = this;
+
+		if (!key) {
+			return new Store();
+		}
+
+		this[Instances] = this[Instances] || new Map();
+
+		if (!this[Instances].has(key)) {
+			this[Instances].set(key, {
+				count: 0,
+				store: new Store(key),
+			});
+		}
+
+		const { store, count } = this[Instances].get(key);
+
+		this[Instances].set(key, { count: count + 1, store });
+
+		return store;
+	}
+
+	static freeStore(key) {
+		if (!this[Instances].has(key)) {
+			return;
+		}
+
+		const { store, count } = this[Instances].get(key);
+
+		if (count <= 1) {
+			this[Instances].delete(key);
+			store.cleanup();
+		} else {
+			this[Instances].set(key, { count: count - 1, store });
+		}
+	}
+
+	static useStore(key) {
+		const getStore = k => this.getStore(k);
+		const freeStore = k => this.freeStore(k);
+
+		const [{ store }, dispatch] = React.useReducer(
+			StoreInstanceReducer,
+			key,
+			initialKey => ({ store: getStore(initialKey) })
+		);
+
+		React.useEffect(() => {
+			const newStore = store[Key] === key ? store : getStore(key);
+
+			if (newStore !== store) {
+				dispatch({ type: 'update', store: newStore });
+			}
+
+			return () => freeStore(key);
+		}, [key]);
+
+		return store;
+	}
+
 	#state = {};
 
-	constructor() {
+	constructor(storeKey) {
 		super();
+
+		this[Key] = storeKey;
 
 		const binding = key => ({
 			scope: this,
@@ -49,9 +126,27 @@ export default class DataStore extends PropertyChangeEmitter {
 
 		this.addDependentProperty('cleaningup', 'cleanup');
 		this.addDependentProperty('cleanedup', 'cleanup');
+
+		this.initialize();
 	}
 
 	//#region Life Cycles
+	#loadAbortController = null;
+	#loadTimeout = null;
+	[Load]() {
+		if (this.#loadAbortController) {
+			this.#loadAbortController.abort();
+		}
+
+		if (!this.#loadTimeout) {
+			this.#loadTimeout = setTimeout(() => {
+				this.#loadAbortController = new AbortController();
+				this.load(this.#loadAbortController);
+				this.#loadTimeout = null;
+			}, 0);
+		}
+	}
+
 	#reader = null;
 	read() {
 		if (!this.#reader) {
@@ -74,7 +169,7 @@ export default class DataStore extends PropertyChangeEmitter {
 				});
 
 				if (!this.load.hasRan) {
-					this.load();
+					this[Load]();
 				}
 			});
 
@@ -112,8 +207,7 @@ export default class DataStore extends PropertyChangeEmitter {
 	get cleanedup() {
 		return this.cleanup.hasRan;
 	}
-
-	//#region
+	//#endregion
 
 	//#region State Updating
 	updateState(newState) {
