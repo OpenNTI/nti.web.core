94c8b8e4125f280eb38860850b69468b9b60c5c1
Ray Hatfield <ray.hatfield@gmail.com>
2021-07-05 16:58:49 -0500

    add string prop value support to getSpacingProps, tests

--- src/system/css/__test__/get-spacing-props.spec.js
+++ src/system/css/__test__/get-spacing-props.spec.js
@@ -2,48 +2,84 @@
 import { getSpacingProps } from '../get-spacing-props.js';
 
 const sizes = ['xs', 'sm', 'md', 'lg', 'xl'];
-const sides = ['l', 't', 'r', 'b', 'h', 'v'];
+const sides = ['t', 'r', 'b', 'l', 'h', 'v'];
 
-// turns names into boolean props,
+// convenience: turns names into boolean props,
 // e.g. 'foo' => { foo: true }
 // or ['foo', 'bar'] => { foo: true, bar: true }
-const k = keys =>
+const b = keys =>
 	[keys].flat().reduce((acc, prop) => ({ ...acc, [prop]: true }), {});
 
 describe('get spacing props', () => {
-	test('padding props map to classnames ', () => {
+	test('boolean padding props map to classnames ', () => {
 		sizes.forEach(size => {
 			// p-md => p-md, etc.
-			expect(getSpacingProps(k(`p-${size}`)).className).toBe(`p-${size}`);
+			expect(getSpacingProps(b(`p-${size}`)).className).toBe(`p-${size}`);
 
 			sides.forEach(side =>
 				// pv-md => pv-md, etc.
-				expect(getSpacingProps(k(`p${side}-${size}`)).className).toBe(
+				expect(getSpacingProps(b(`p${side}-${size}`)).className).toBe(
 					`p${side}-${size}`
 				)
 			);
 		});
 	});
 
-	test('margin props map to classnames', () => {
+	test('boolean margin props map to classnames', () => {
 		sizes.forEach(size => {
 			// m-sm => m-sm, etc.
-			expect(getSpacingProps(k(`m-${size}`)).className).toBe(`m-${size}`);
+			expect(getSpacingProps(b(`m-${size}`)).className).toBe(`m-${size}`);
 
 			sides.forEach(side =>
 				// mv-sm => mv-sm, etc.
-				expect(getSpacingProps(k(`m${side}-${size}`)).className).toBe(
+				expect(getSpacingProps(b(`m${side}-${size}`)).className).toBe(
 					`m${side}-${size}`
 				)
 			);
 		});
 	});
 
-	test('multple props map to multple classnames', () => {
-		const { className } = getSpacingProps(k(['p-md', 'mh-lg']));
+	test('multple boolean props map to multple classnames', () => {
+		const { className } = getSpacingProps(b(['p-md', 'mh-lg']));
 		const names = className.split(' ');
 		expect(names).toHaveLength(2);
 		expect(names[0]).toBe('p-md');
 		expect(names[1]).toBe('mh-lg');
+	});
+
+	test('string padding props map to classnames', () => {
+		sizes.forEach(size => {
+			// { p: 'md' } => { className: 'p-md' }
+			expect(getSpacingProps({ p: size })).toHaveProperty(
+				'className',
+				`p-${size}`
+			);
+
+			sides.forEach(side => {
+				// { pv: 'xl' } => { className: 'pv-xl' }
+				expect(getSpacingProps({ [`p${side}`]: size })).toHaveProperty(
+					'className',
+					`p${side}-${size}`
+				);
+			});
+		});
+	});
+
+	test('string margin props map to classnames', () => {
+		sizes.forEach(size => {
+			// { m: 'md' } => { className: 'm-md' }
+			expect(getSpacingProps({ m: size })).toHaveProperty(
+				'className',
+				`m-${size}`
+			);
+
+			sides.forEach(side => {
+				// { pv: 'xl' } => { className: 'pv-xl' }
+				expect(getSpacingProps({ [`m${side}`]: size })).toHaveProperty(
+					'className',
+					`m${side}-${size}`
+				);
+			});
+		});
 	});
 });
--- src/system/css/get-spacing-props.js
+++ src/system/css/get-spacing-props.js
@@ -6,12 +6,26 @@ const mapping = {
 	m,
 };
 
+function classFromPropName(propName) {
+	const type = propName.charAt(0);
+	return mapping[type]?.[propName];
+}
+
+export function classFromProp(name, value) {
+	// { 'p-md': true } via <Cmp p-md />
+	if (value === true) {
+		return classFromPropName(name);
+	}
+
+	// { p: 'md' } via <Cmp p="md" />
+	const type = name.charAt(0);
+	const key = `${name}-${value}`; // 'p-md'
+	return mapping[type]?.[key];
+}
+
 export function getSpacingProps(props, defaults) {
-	const className = Object.keys(props)
-		.map(prop => {
-			const type = prop.charAt(0);
-			return mapping[type]?.[prop];
-		})
+	const className = Object.entries(props)
+		.map(([prop, value]) => classFromProp(prop, value))
 		.filter(Boolean)
 		.join(' ');
 
