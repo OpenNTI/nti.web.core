7004cb16cb10c8f731c22994b3f60c676a100265
Andrew Ligon <aligon90@gmail.com>
2021-07-09 16:46:43 -0500

    add tests and documentation for useRead

--- src/data/hooks/use-read.js
+++ src/data/hooks/use-read.js
@@ -1,7 +1,16 @@
+/** @typedef {(store: import('./use-store').StoreInstance) => boolean} Predicate */
+/** @typedef {Predicate|import('./use-store').StoreInstance} PredicateOrStore */
+
 import Context from '../Context';
 
 const Identity = x => x;
 
+/**
+ * Return the read of the store instance or the instance from context that matches the predicate.
+ *
+ * @param {PredicateOrStore} predicate - store instance or predicate to match a store instance from the context
+ * @returns {*}
+ */
 export default function useRead(predicate) {
 	const { stores } = Context.useContext();
 	const filtered = predicate?.read
@@ -9,6 +18,8 @@ export default function useRead(predicate) {
 		: stores.filter(predicate ?? Identity);
 
 	const store = filtered[filtered.length - 1];
+
+	//TODO: figure out how to listen for new reads on the store
 
 	return store?.read();
 }
