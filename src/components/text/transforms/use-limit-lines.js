ed8e76fbe88e0faa64006102625fd2bc08318569
Andrew Ligon <aligon90@gmail.com>
2021-07-18 23:18:20 -0500

    checkpoint for text component

---
+++ src/components/text/transforms/use-limit-lines.js
@@ -0,0 +1,54 @@
+import { useEffect, useState } from 'react';
+import cx from 'classnames';
+
+const styles = stylesheet`
+	.multi-line-limit {
+		display: -webkit-box !important;
+		-webkit-line-clamp: var(--line-limit);
+		-webkit-box-orient: vertical;
+		overflow: hidden;
+		text-overflow: ellipsis;
+		max-height: calc(var(--line-limit) * var(--line-height));
+
+		/* The eventual standard:
+		https://drafts.csswg.org/css-overflow-3/#propdef-line-clamp */
+		line-clamp: var(--line-limit) "…";
+	}
+
+	.single-line-limit {
+		display: block;
+		white-space: nowrap;
+		overflow: hidden;
+		text-overflow: ellipsis;
+	}
+`;
+
+export default function useLimitLines(
+	ref,
+	{ className, style, limitLines, limit, ...otherProps }
+) {
+	const [lineHeight, setLineHeight] = useState('1em');
+
+	const isSingleLine = limit === 1;
+	const isMultiLine = limit > 1;
+
+	useEffect(() => {
+		if (isMultiLine) {
+			setLineHeight(getComputedStyle(ref.current).lineHeight);
+		}
+	}, [className]);
+
+	return {
+		...otherProps,
+		className: cx(className, {
+			[styles.multiLineLimit]: isMultiLine,
+			[styles.singeLineLimit]: isSingleLine,
+		}),
+		style: {
+			...style,
+			...(isMultiLine
+				? { '--line-limit': limit, '--line-height': lineHeight }
+				: null),
+		},
+	};
+}
