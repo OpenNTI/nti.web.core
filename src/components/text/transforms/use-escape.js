e3d278d8b4b11a2a57adca78feb6acf3460470ab
Andrew Ligon <aligon90@gmail.com>
2021-08-19 13:22:19 -0500

    handle multiple children passed to the text component

--- src/components/text/transforms/use-escape.js
+++ src/components/text/transforms/use-escape.js
@@ -3,16 +3,19 @@ import { useLayoutEffect, useState } from 'react';
 export default function useEscape(ref, { allowMarkup, text, ...otherProps }) {
 	const [escaped, setEscaped] = useState(text);
 
-	useLayoutEffect(() => {
-		if (typeof text !== 'string' || allowMarkup) {
-			setEscaped(text);
-			return;
-		}
+	useLayoutEffect(
+		() => {
+			if (typeof text !== 'string' || allowMarkup) {
+				setEscaped(text);
+				return;
+			}
 
-		const span = document.createElement('span');
-		span.appendChild(document.createTextNode(text));
-		setEscaped(span.innerHTML);
-	}, [text]);
+			const span = document.createElement('span');
+			span.appendChild(document.createTextNode(text));
+			setEscaped(span.innerHTML);
+		},
+		Array.isArray(text) ? text : [text]
+	);
 
 	return {
 		...otherProps,
