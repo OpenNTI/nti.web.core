cb16a6b7853f66c8a9d1e397c08fad2e283dab04
Jonathan Grimes <jsg2021@gmail.com>
2021-09-08 12:53:46 -0500

    fix ref warning

--- src/components/list/Inline.jsx
+++ src/components/list/Inline.jsx
@@ -9,27 +9,24 @@ const Middot = (
 	</span>
 );
 
-export function InlineList({
-	children,
-	className,
-	separator = Middot,
-	...otherProps
-}) {
-	const items = React.Children.toArray(children).filter(
-		x => x !== false && x != null
-	);
+export const InlineList = React.forwardRef(
+	({ children, className, separator = Middot, ...otherProps }, ref) => {
+		const items = React.Children.toArray(children).filter(
+			x => x !== false && x != null
+		);
 
-	return (
-		<ul className={cx(className, Theme.inlineList)} {...otherProps}>
-			{items.map((item, index, list) => (
-				<li key={index}>
-					{item}
-					{!isLast(index, list) && separator}
-				</li>
-			))}
-		</ul>
-	);
-}
+		return (
+			<ul className={cx(className, Theme.inlineList)} {...otherProps}>
+				{items.map((item, index, list) => (
+					<li key={index}>
+						{item}
+						{!isLast(index, list) && separator}
+					</li>
+				))}
+			</ul>
+		);
+	}
+);
 
 function isLast(index, { length }) {
 	return index >= length - 1;
