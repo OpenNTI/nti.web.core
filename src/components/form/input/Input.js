2f65146854bf81dd6512b9e8b9afb1c6bd8371f4
Andrew Ligon <aligon90@gmail.com>
2021-10-21 23:41:24 -0500

    expose the option component

--- src/components/form/input/Input.js
+++ src/components/form/input/Input.js
@@ -5,6 +5,7 @@ import Theme from './Input.theme.css';
 
 export { Checkbox } from './Checkbox';
 export { ListBox } from './lists/ListBox';
+export { Option } from './lists/Option';
 export const Placeholder = Variant(Text, { className: Theme.placeholder });
 export { Select } from './lists/Select';
 export { Text } from './Text';
