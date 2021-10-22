9578b97c2b98062937e3ef7febe9a16ccbb0a407
Jonathan Grimes <jsg2021@gmail.com>
2021-10-22 15:36:34 -0500

    add missing circled

--- src/components/icons/Check.jsx
+++ src/components/icons/Check.jsx
@@ -1,6 +1,6 @@
-
 import { setIcon } from './types/identity';
 import FontIcon from './types/Font-Icon';
+import SVGIcon from './types/SVG-Icon';
 
 const icon = 'icon-check';
 
@@ -9,3 +9,12 @@ function CheckIcon(props) {
 }
 
 export const Check = setIcon(CheckIcon);
+
+Check.Circled = setIcon(props => (
+	<SVGIcon width="24" height="24" {...props} viewBox="0 0 24 24">
+		<g fill="currentColor" fillRule="evenodd">
+			<path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm0-1.5a8.5 8.5 0 1 0 0-17 8.5 8.5 0 0 0 0 17z" />
+			<path d="m8 13.318.994-1.699 2.468 1.884L14.476 8 16 8.9 11.974 16z" />
+		</g>
+	</SVGIcon>
+));
